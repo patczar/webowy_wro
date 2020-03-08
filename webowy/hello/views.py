1d6c9c5dbc240b66d41c70a285b8ada591efe948
Patryk Czarnik <patryk@patrykczarnik.net>
2020-03-08 15:42:44 +0100

    Kalkulator - obsługa innych działań

--- webowy/hello/views.py
+++ webowy/hello/views.py
@@ -97,13 +97,18 @@ def rozmowa_szablon(request:HttpRequest) -> HttpResponse:
 
 
 def kalkulator(request:HttpRequest) -> HttpResponse:
+    operacja = request.GET.get('operacja')
     arg1 = request.GET.get('arg1')
     arg2 = request.GET.get('arg2')
 
-    if arg1 and arg2:
+    if operacja and arg1 and arg2:
         liczba1 = int(arg1)
         liczba2 = int(arg2)
-        wynik = liczba1 + liczba2
+        if operacja == 'add': wynik = liczba1 + liczba2
+        elif operacja == 'sub': wynik = liczba1 - liczba2
+        elif operacja == 'mul': wynik = liczba1 * liczba2
+        elif operacja == 'div': wynik = liczba1 / liczba2
+        else: wynik = 0
     else:
         wynik = 0
 
