b47cbe46c40a2fb5fe0639480afcb74eeab54555
Patryk Czarnik <patryk@patrykczarnik.net>
2020-03-08 16:42:17 +0100

    Model sklepu

--- webowy/sklep/admin.py
+++ webowy/sklep/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+# Z modułu models znajdującego się bieżącym pakiecie (sklep)
+# importuję wszystkie klasy
+from .models import *
+
 # Register your models here.
+admin.site.register(Produkt)
+admin.site.register(Zamowienie)
+admin.site.register(TowarWZamowieniu)
--- webowy/sklep/models.py
+++ webowy/sklep/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Produkt(models.Model):
+    nazwa = models.CharField(max_length=100)
+    cena = models.DecimalField(max_digits=10, decimal_places=2)
+    promocja = models.BooleanField()
+
+
+class Zamowienie(models.Model):
+    data = models.DateTimeField()
+    adres_ulica = models.CharField(max_length=100)
+    adres_miasto = models.CharField(max_length=50)
+    adres_kod = models.CharField(max_length=10)
+
+
+class TowarWZamowieniu(models.Model):
+    zamowienie = models.ManyToManyField(Zamowienie)
+    produkt = models.ManyToManyField(Produkt)
+    ilosc = models.IntegerField()
